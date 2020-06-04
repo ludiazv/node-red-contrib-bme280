00dfad129ec9b381cbcc74482b54b5a0b94e18ff
L Diaz <ldiaz@navarone.local>
2020-06-04 17:01:11 +0200

    Correction altitude calculation #5

--- bme280.js
+++ bme280.js
@@ -11,6 +11,7 @@ module.exports = function (RED) {
         node.addr = parseInt(n.address, 16);
         node.topic = n.topic || "";
         node.extra = n.extra || false;
+        node.preasure= parseFloat(n.preasure) || 1013.25;
         node.initialized = false;
         node.isBME = false;
 
@@ -18,34 +19,28 @@ module.exports = function (RED) {
         node.status({ fill: "grey", shape: "ring", text: "Init..." });
         node.log("Initializing on bus" + node.bus + " addr:" + node.addr);
         node.sensor = new BME280({ i2cBusNo: node.bus, i2cAddress: node.addr });
-        node.sensor.init().then(function (ID) {
-            node.initialized = true;
-            node.type = ID == BME280.CHIP_ID_BME280() ? "BME280" : "BMP280";
-            node.isBME = (node.type == "BME280");
-            node.status({ fill: "green", shape: "dot", text: node.type + " ready" });
-            node.log("Sensor " + node.type + " initialized.");
-        }).catch(function (err) {
-            node.status({ fill: "red", shape: "ring", text: "Sensor Init Failed" });
-            node.error("Sensor Init failed ->" + err);
-        });
+        var fnInit= function() {
+            node.sensor.init().then(function (ID) {
+                node.initialized = true;
+                node.type = ID == BME280.CHIP_ID_BME280() ? "BME280" : "BMP280";
+                node.isBME = (node.type == "BME280");
+                node.status({ fill: "green", shape: "dot", text: node.type + " ready" });
+                node.log("Sensor " + node.type + " initialized.");
+            }).catch(function (err) {
+                node.status({ fill: "red", shape: "ring", text: "Sensor Init Failed" });
+                node.error("Sensor Init failed ->" + err);
+            });
+        };
+        // Init
+        fnInit();
         // trigger measure
         node.on('input', function (_msg) {
             if (!node.initialized) {
                 //try to reinit node until no sensor is found
-                node.sensor.init().then(function (ID) {
-                    node.initialized = true;
-                    node.type = ID == BME280.CHIP_ID_BME280() ? "BME280" : "BMP280";
-                    node.isBME = (node.type == "BME280");
-                    node.status({ fill: "green", shape: "dot", text: node.type + " ready" });
-                    node.log("Sensor " + node.type + " initialized.");
-                }).catch(function (err) {
-                    node.status({ fill: "red", shape: "ring", text: "Sensor Init Failed" });
-                    node.error("Sensor Init failed ->" + err);
-                });
+                fnInit();
                 return null;
             }
             node.sensor.readSensorData().then(function (data) {
-
                 _msg.payload = data;
                 data.model = node.type;
                 if (!node.isBME && _msg.payload.humidity !== undefined) delete _msg.payload.humidity;
@@ -56,7 +51,7 @@ module.exports = function (RED) {
                         pl.heatIndex = BME280.calculateHeatIndexCelcius(data.temperature_C, data.humidity);
                         pl.dewPoint_C = BME280.calculateDewPointCelcius(data.temperature_C, data.humidity);
                     }
-                    pl.altitude_M = BME280.calculateAltitudeMeters(data.pressure_hPa, 0);
+                    pl.altitude_M = BME280.calculateAltitudeMeters(data.pressure_hPa, node.preasure);
                     pl.temperature_F = BME280.convertCelciusToFahrenheit(data.temperature_C);
                     pl.pressure_Hg = BME280.convertHectopascalToInchesOfMercury(data.pressure_hPa);
                 }
