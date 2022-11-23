3a1f00d30e224fc735986e71a662e9213a398c96
ludiazv <ldiaz@atloideas.com>
2022-11-23 10:54:28 +0100

    added max init error retries to avoid abuse of I2c

--- bme280.js
+++ bme280.js
@@ -2,6 +2,7 @@
 
 module.exports = function (RED) {
     const BME280 = require('bme280-sensor');
+    const MAX_INIT_ERRORS=50;
 
     function Bme280(n) {
         RED.nodes.createNode(this, n);
@@ -13,6 +14,7 @@ module.exports = function (RED) {
         node.extra = n.extra || false;
         node.preasure= parseFloat(n.preasure) || 1013.25;
         node.initialized = false;
+        node.init_errors= 0;
         node.isBME = false;
 
         // init the sensor
@@ -27,8 +29,13 @@ module.exports = function (RED) {
                 node.status({ fill: "green", shape: "dot", text: node.type + " ready" });
                 node.log("Sensor " + node.type + " initialized.");
             }).catch(function (err) {
+                node.initialized=false;
+                node.init_errors++;
                 node.status({ fill: "red", shape: "ring", text: "Sensor Init Failed" });
-                node.error("Sensor Init failed ->" + err);
+                node.error("Sensor Init failed [" + node.init_errors + "]-> " + err);
+                if(node.init_errors > MAX_INIT_ERRORS) {
+                    node.error("Init failed more than " + MAX_INIT_ERRORS + " times. The senser will remain in failed stated.");
+                }
             });
         };
         // Init
@@ -36,33 +43,37 @@ module.exports = function (RED) {
         // trigger measure
         node.on('input', function (_msg) {
             if (!node.initialized) {
-                //try to reinit node until no sensor is found
-                fnInit();
-                return null;
+                //try to reinit node until no sensor is found with max retries
+                if(node.init_errors <= MAX_INIT_ERRORS) fnInit();
             }
-            node.sensor.readSensorData().then(function (data) {
-                _msg.payload = data;
-                data.model = node.type;
-                if (!node.isBME && _msg.payload.humidity !== undefined) delete _msg.payload.humidity;
-                if (node.topic !== undefined && node.topic != "") _msg.topic = node.topic;
-                if (node.extra) {
-                    var pl = _msg.payload;
-                    if (node.isBME) {
-                        pl.heatIndex = BME280.calculateHeatIndexCelcius(data.temperature_C, data.humidity);
-                        pl.dewPoint_C = BME280.calculateDewPointCelcius(data.temperature_C, data.humidity);
+            if (!node.initialized) {
+                node.send(_msg); // msg bypass
+            } else {
+                node.sensor.readSensorData().then(function (data) {
+                    _msg.payload = data;
+                    data.model = node.type;
+                    if (!node.isBME && _msg.payload.humidity !== undefined) delete _msg.payload.humidity;
+                    if (node.topic !== undefined && node.topic != "") _msg.topic = node.topic;
+                    if (node.extra) {
+                        var pl = _msg.payload;
+                        if (node.isBME) {
+                            pl.heatIndex = BME280.calculateHeatIndexCelcius(data.temperature_C, data.humidity);
+                            pl.dewPoint_C = BME280.calculateDewPointCelcius(data.temperature_C, data.humidity);
+                        }
+                        pl.altitude_M = BME280.calculateAltitudeMeters(data.pressure_hPa, node.preasure);
+                        pl.temperature_F = BME280.convertCelciusToFahrenheit(data.temperature_C);
+                        pl.pressure_Hg = BME280.convertHectopascalToInchesOfMercury(data.pressure_hPa);
                     }
-                    pl.altitude_M = BME280.calculateAltitudeMeters(data.pressure_hPa, node.preasure);
-                    pl.temperature_F = BME280.convertCelciusToFahrenheit(data.temperature_C);
-                    pl.pressure_Hg = BME280.convertHectopascalToInchesOfMercury(data.pressure_hPa);
-                }
-                node.send(_msg);
-                var sText = node.type + "[Tcº:" + Math.round(data.temperature_C);
-                if (node.isBME) sText += ("/H%:" + Math.round(data.humidity));
-                node.status({ fill: "green", shape: "dot", text: sText + "]" });
-            }).catch(function (err) {
-                node.status({ fill: "red", shape: "ring", text: "Sensor reading failed" });
-                node.error("Failed to read data ->" + err);
-            });
+                    node.send(_msg);
+                    var sText = node.type + "[Tcº:" + Math.round(data.temperature_C);
+                    if (node.isBME) sText += ("/H%:" + Math.round(data.humidity));
+                    node.status({ fill: "green", shape: "dot", text: sText + "]" });
+                }).catch(function (err) {
+                    node.status({ fill: "red", shape: "ring", text: "Sensor reading failed" });
+                    node.error("Failed to read data ->" + err);
+                    node.send(_msg); // msg bypass
+                });
+            }
             return null;
         });
 
